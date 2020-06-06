b3c92269582dc1807d1df65d91579cac22aa46ee
unknown <shumailmehmood1@gmail.com>
2020-06-06 22:47:20 +0500

    C

---
+++ build/precache-manifest.625d18a2e7ecd6e33adbc3979e0498a7.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4fb6d21fcd5b02567023620cd8552e3",
+    "url": "/now-ui-dashboard-react/index.html"
+  },
+  {
+    "revision": "ccb37a65732f458bcbb8",
+    "url": "/now-ui-dashboard-react/static/css/2.0cee8fd6.chunk.css"
+  },
+  {
+    "revision": "6a190249cb7fae42a5ed",
+    "url": "/now-ui-dashboard-react/static/css/main.6d94e8dc.chunk.css"
+  },
+  {
+    "revision": "ccb37a65732f458bcbb8",
+    "url": "/now-ui-dashboard-react/static/js/2.28de9cfd.chunk.js"
+  },
+  {
+    "revision": "6a190249cb7fae42a5ed",
+    "url": "/now-ui-dashboard-react/static/js/main.736eaa37.chunk.js"
+  },
+  {
+    "revision": "5c5c1d61a25fa76a6548",
+    "url": "/now-ui-dashboard-react/static/js/runtime~main.6231c9ca.js"
+  },
+  {
+    "revision": "eec7c7f60134e712ef7174c96ca7ee5a",
+    "url": "/now-ui-dashboard-react/static/media/logo-white.eec7c7f6.svg"
+  },
+  {
+    "revision": "22a0bffe789c286a9d78eb52670996a7",
+    "url": "/now-ui-dashboard-react/static/media/nucleo-outline.22a0bffe.ttf"
+  },
+  {
+    "revision": "24e2d6b43b1b0f84fdfaa06a4032f154",
+    "url": "/now-ui-dashboard-react/static/media/nucleo-outline.24e2d6b4.woff"
+  },
+  {
+    "revision": "53a1bed7a3ec86d010fe100873828a89",
+    "url": "/now-ui-dashboard-react/static/media/nucleo-outline.53a1bed7.eot"
+  },
+  {
+    "revision": "8ebec31f5ce59f908db84d86aed5947f",
+    "url": "/now-ui-dashboard-react/static/media/nucleo-outline.8ebec31f.woff2"
+  }
+]);
